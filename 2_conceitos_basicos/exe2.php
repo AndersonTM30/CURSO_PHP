bf9766b8862980fda99e8fbb92e2bb9312af81e6
AndersonTM30 <andersondev30@gmail.com>
2022-12-02 22:45:50 -0300

    Exercício 2

---
+++ 2_conceitos_basicos/exe2.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+    Descreva características básicas de uma pessoaou objeto utilizando comentários, pelo menos três, em linhas
+*/
+
+//  Olhos, braços, pernas
+# Pelos, calda, língua
+/* Alto, largo, pesado */
