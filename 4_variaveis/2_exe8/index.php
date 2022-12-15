b42cdf8110df24321e3d72361d5362e59bd1856a
AndersonTM30 <andersondev30@gmail.com>
2022-12-15 04:36:42 -0300

    feat - Exercício número 8: variáveis

---
+++ 4_variaveis/2_exe8/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Crie três variáveis com tipos de dados diferentes e imprima no navegador.
+
+$nome = 'Anderson';
+
+$idade = 30;
+
+$casado = true;
+
+echo "O colaborador $nome, tem $idade anos e é casado?(1 = Sim, 0 = Não) $casado";
