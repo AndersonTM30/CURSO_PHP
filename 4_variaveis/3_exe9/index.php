6c16a8434be643c8d1dbc1856e9f2efcb80ea67d
AndersonTM30 <andersondev30@gmail.com>
2022-12-15 04:43:01 -0300

    feat - Exercício número 9: variáveis

---
+++ 4_variaveis/3_exe9/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// Crie duas variáveis do tipo inteiro e retorne a soma das duas em uma terceira variável e imprima na tela
+
+$numero1 = 30;
+$numero2 = 15;
+$resultado = $numero1 + $numero2;
+
+echo "$numero1 + $numero2 = $resultado";
