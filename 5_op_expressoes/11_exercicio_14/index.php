e346f9b3af3bb1618325dff571e1fa9c350d48df
AndersonTM30 <andersondev30@gmail.com>
2022-12-23 04:48:45 -0300

    feat - Exercício 14

---
+++ 5_op_expressoes/11_exercicio_14/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Crie uma variável saudação, nome e outra de sobrenome;
+    Imprima com echo a concatenação de saudação, nome e sobrenome;
+*/
+
+$saudacao = "Seja bem vindo ao curso de PHP";
+$nome = "Anderson";
+$sobrenome = "Ferreira";
+
+echo "$saudacao $nome $sobrenome!";
