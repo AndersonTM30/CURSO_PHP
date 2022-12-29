cf3d92c4641307e1cddda9e81595dac4eadc318d
AndersonTM30 <andersondev30@gmail.com>
2022-12-28 22:02:20 -0300

    feat - Exercício 15

---
+++ 5_op_expressoes/14_exercicio_15/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Crie uma operação que retorne false com igualdade;
+//  Crie uma operação que retorne verdadeiro com igualdade;
+
+$num1 = 10;
+$num2 = 11;
+
+if($num1 != $num2) {
+    echo "Os valores de $num1 e $num2 são diferens" . PHP_EOL;
+}
+
+$a = 5;
+$b = 5;
+
+if ($a == $b) {
+    echo "Os valores de $a e $b são iguals" . PHP_EOL;
+}
