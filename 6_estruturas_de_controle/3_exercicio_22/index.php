e706802f6d8ea03f9dac39881cd648994e98326f
AndersonTM30 <andersondev30@gmail.com>
2023-01-02 04:52:03 -0300

    feat - Exercício 22

---
+++ 6_estruturas_de_controle/3_exercicio_22/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+    Crie variáveis que recebam idades;
+    Cheque se as idades são maiores ou iguais a 18;
+    Se sim, imprima uma mensagem que a pessoa é maior de idade;
+*/
+
+$a = 15;
+$b = 19;
+$c = 26;
+
+if ($a >= 18) {
+    echo "A pessoa é maior de idade!" . PHP_EOL;
+}
+
+if ($b >= 18) {
+    echo "A pessoa é maior de idade 2!" . PHP_EOL;
+}
+
+if ($c >= 18) {
+    echo "A pessoa é maior de idade 3!" . PHP_EOL;
+}
