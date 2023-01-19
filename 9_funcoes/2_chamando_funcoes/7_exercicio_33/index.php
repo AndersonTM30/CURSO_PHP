198c3803b84cc1f72dc5c593a751087671a079da
AndersonTM30 <andersondev30@gmail.com>
2023-01-19 07:47:08 -0300

    feat - Exercício 33

---
+++ 9_funcoes/2_chamando_funcoes/7_exercicio_33/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Crie uma função que deve receber como parâmetros o nome e a idade;
+ * Imprima "Olá eu sou o NOME e tenho X anos";
+ */
+
+ function pessoa($nome, $idade)
+ {
+    echo "Olá eu sou $nome e tenho $idade anos." . PHP_EOL;
+ }
+
+ pessoa("Anderson", 30);
